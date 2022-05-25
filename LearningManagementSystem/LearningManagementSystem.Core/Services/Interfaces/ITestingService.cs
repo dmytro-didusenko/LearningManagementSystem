d8bfb86c2516246cf4db38cdab7ff14b4175c632
maksym-snizhynskyi <Maksym.Snizhynskyi@fivesysdev.com>
2022-05-25 17:31:45 +0300

    restructured Models folder

---
+++ LearningManagementSystem/LearningManagementSystem.Core/Services/Interfaces/ITestingService.cs
@@ -0,0 +1,11 @@
+﻿using LearningManagementSystem.Domain.Models;
+using LearningManagementSystem.Domain.Models.Testing;
+
+namespace LearningManagementSystem.Core.Services.Interfaces
+{
+    public interface ITestingService
+    {
+        public Task<Response<TestModel>> CreateTestAsync(TestModel model);
+    }
+    
+}

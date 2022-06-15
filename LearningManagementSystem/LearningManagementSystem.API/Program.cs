821ac12a6dfaa7c9e9f51abd3fdcc2d50577eeb8
maksym-snizhynskyi <Maksym.Snizhynskyi@fivesysdev.com>
2022-06-15 15:33:25 +0300

    fixed code style issue

--- LearningManagementSystem/LearningManagementSystem.API/Program.cs
+++ LearningManagementSystem/LearningManagementSystem.API/Program.cs
@@ -49,13 +49,12 @@ builder.Services.AddMassTransit(cfg =>
         x.Host(new Uri(builder.Configuration["RabbitMQ:Uri"]));
     });
 });
-Adding Quartz
+//Adding Quartz
 builder.Services.AddQuartz(cfg =>
 {
     cfg.UseMicrosoftDependencyInjectionJobFactory();
-cfg.AddJobAndTrigger<BirthdayGreetingJob>(builder.Configuration);
-cfg.AddJobAndTrigger<CourseStartingJob>(builder.Configuration);
-
+    cfg.AddJobAndTrigger<BirthdayGreetingJob>(builder.Configuration);
+    cfg.AddJobAndTrigger<CourseStartingJob>(builder.Configuration);
 });
 builder.Services.AddQuartzHostedService(cfg => cfg.WaitForJobsToComplete = true);
 

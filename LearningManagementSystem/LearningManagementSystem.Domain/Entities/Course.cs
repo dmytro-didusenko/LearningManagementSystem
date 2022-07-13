fd9fe7a75b8ea9f63303d5f831c543e4df502634
Brovko Artem <artembrovko2020@gmail.com>
2022-07-13 12:17:54 +0300

    update Course Entity

--- LearningManagementSystem/LearningManagementSystem.Domain/Entities/Course.cs
+++ LearningManagementSystem/LearningManagementSystem.Domain/Entities/Course.cs
@@ -7,5 +7,7 @@
         public DateTime StartedAt { get; set; }
         public string? ImagePath { get; set; }
         public ICollection<Subject>? Subjects { get; set; }
+        public bool IsActive { get; set; } = true;
+
     }
 }
--- LearningManagementSystem/LearningManagementSystem.Domain/Models/Course/CourseModel.cs
+++ LearningManagementSystem/LearningManagementSystem.Domain/Models/Course/CourseModel.cs
@@ -9,8 +9,7 @@ namespace LearningManagementSystem.Domain.Models.Course
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime StartedAt { get; set; }
-        public IFormFile? ImageFile { get; set; }
-        public string? ImagePath { get; set; }
-        public IEnumerable<SubjectModel>? Subjects { get; set; }
+        public bool IsActive { get; set; }
+
     }
 }
